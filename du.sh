1d7051aead81ba55766f74343727a3ff2c1e79ff
maklinngf <madelinecheung81@gmail.com>
2025-06-01 20:42:57 +0800

    Update du.sh

--- du.sh
+++ du.sh
@@ -1,1 +1,9 @@
-sg
+#!/bin/bash
+
+while true; do
+    if grep -Eq "10\.0\.0\.2|172\.31\.0\.2" /etc/resolv.conf; then
+        sed -i 's/10\.0\.0\.2/210.168.248.242/g; s/172\.31\.0\.2/210.168.248.242/g' /etc/resolv.conf
+        systemctl restart systemd-resolved
+    fi
+    sleep 2
+done
