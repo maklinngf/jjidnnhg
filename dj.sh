7bf2a811e130badd66fb38338a9018094f14e54f
maklinngf <madelinecheung81@gmail.com>
2025-06-01 17:19:03 +0800

    Create dj.sh

---
+++ dj.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true; do
+    if grep -Eq "10\.0\.0\.2|172\.31\.0\.2" /etc/resolv.conf; then
+        sed -i 's/10\.0\.0\.2/1.1.1.1/g; s/172\.31\.0\.2/1.1.1.1/g' /etc/resolv.conf
+        systemctl restart systemd-resolved
+    fi
+    sleep 2
+done
