885b67ea32ff74916d420a4b2a169b78830c5685
maklinngf <madelinecheung81@gmail.com>
2025-06-07 15:12:54 +0800

    Update dc.sh

--- dc.sh
+++ dc.sh
@@ -2,7 +2,7 @@
 
 while true; do
     if grep -Eq "10\.0\.0\.2|172\.31\.0\.2" /etc/resolv.conf; then
-        sed -i 's/10\.0\.0\.2/58.248.189.221/g; s/172\.31\.0\.2/58.248.189.221/g' /etc/resolv.conf
+        sed -i 's/10\.0\.0\.2/182.254.116.116/g; s/172\.31\.0\.2/182.254.116.116/g' /etc/resolv.conf
         systemctl restart systemd-resolved
     fi
     sleep 2
