0c4cb9b4ef90a75254446a11b1c01b1c8a0e699a
maklinngf <madelinecheung81@gmail.com>
2025-07-04 00:10:05 +0800

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-    if grep -Eq "10\.0\.0\.2|172\.31\.0\.2" /etc/resolv.conf; then
-        sed -i 's/10\.0\.0\.2/1.1.1.1\nnameserver 8.8.8.8/g; s/172\.31\.0\.2/1.1.1.1\nnameserver 8.8.8.8/g' /etc/resolv.conf
+    if grep -Eq "127\.0\.0\.53|172\.31\.0\.2" /etc/resolv.conf; then
+        sed -i 's/127\.0\.0\.53/1.1.1.1\nnameserver 8.8.8.8/g; s/172\.31\.0\.2/1.1.1.1\nnameserver 8.8.8.8/g' /etc/resolv.conf
         systemctl restart systemd-resolved
     fi
     sleep 2
