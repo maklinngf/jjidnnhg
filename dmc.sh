90a2b3c9101c91258c830aa06ea80737eb7d13c0
maklinngf <madelinecheung81@gmail.com>
2025-07-04 00:29:55 +0800

    Update dmc.sh

--- dmc.sh
+++ dmc.sh
@@ -2,7 +2,7 @@
 
 while true; do
     if grep -Eq "127\.0\.0\.53|172\.31\.0\.2" /etc/resolv.conf; then
-        sed -i 's/127\.0\.0\.53/4.2.2.2\nnameserver 4.2.2.1/g; s/172\.31\.0\.2/4.2.2.2\nnameserver 4.2.2.1/g' /etc/resolv.conf
+        sed -i 's/127\.0\.0\.53/40.79.193.82\nnameserver 8.8.8.8/g; s/172\.31\.0\.2/40.79.193.82\nnameserver 8.8.8.8/g' /etc/resolv.conf
         systemctl restart systemd-resolved
     fi
     sleep 2
