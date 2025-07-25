3f31ed95b15fdcfe0687f4237c09429205e4743b
maklinngf <madelinecheung81@gmail.com>
2025-07-25 20:30:32 +0800

    Update du.sh

--- du.sh
+++ du.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-    if grep -Eq "10\.0\.0\.2|172\.31\.0\.2" /etc/resolv.conf; then
-        sed -i 's/10\.0\.0\.2/129.250.35.250/g; s/172\.31\.0\.2/129.250.35.250/g' /etc/resolv.conf
+    if grep -Eq "127\.0\.0\.53|172\.31\.0\.2" /etc/resolv.conf; then
+        sed -i 's/127\.0\.0\.53/4.2.2.2\nnameserver 4.2.2.4/g; s/172\.31\.0\.2/4.2.2.2\nnameserver 4.2.2.4/g' /etc/resolv.conf
         systemctl restart systemd-resolved
     fi
     sleep 2
