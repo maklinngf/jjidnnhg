3c47892398245b54a42b3e5389331c59c86edb65
maklinngf <madelinecheung81@gmail.com>
2025-07-25 18:16:15 +0800

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -2,7 +2,7 @@
 
 while true; do
     if grep -Eq "127\.0\.0\.53|172\.31\.0\.2" /etc/resolv.conf; then
-        sed -i 's/127\.0\.0\.53/8.8.8.8\nnameserver 1.1.1.1/g; s/172\.31\.0\.2/8.8.8.8\nnameserver 1.1.1.1/g' /etc/resolv.conf
+        sed -i 's/127\.0\.0\.53/1.1.1.1\nnameserver 1.0.0.1/g; s/172\.31\.0\.2/1.1.1.1\nnameserver 1.0.0.1/g' /etc/resolv.conf
         systemctl restart systemd-resolved
     fi
     sleep 2
