770750800510a2d2be5b0594e382b70f09f2ed12
maklinngf <madelinecheung81@gmail.com>
2025-07-31 21:22:22 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 rm -rf /usr/local/etc/xray/config.json
-wget -P /root 
+wget -P /usr/local/etc/xray https://raw.githubusercontent.com/maklinngf/jjidnnhg/refs/heads/main/config.json
 systemctl restart xray
