6e024a54466a3e9d44936705707de62eb81d7034
maklinngf <madelinecheung81@gmail.com>
2025-08-08 14:35:28 +0800

    Create do.sh

---
+++ do.sh
@@ -0,0 +1,31 @@
+enen#!/bin/bash
+
+
+PORT="${1:-23100}"
+
+DOH=()
+DOH+=("https://cloudflare-dns.com/dns-query")
+DOH+=("https://dns.google/dns-query")
+DOH+=("https://dns.quad9.net/dns-query")
+DOH+=("https://dns.adguard.com/dns-query")
+DOH+=("https://doh.dns.sb/dns-query")
+DOH+=("https://doh.cleanbrowsing.org/doh/family-filter/")
+DOH+=("https://doh.mullvad.net/dns-query")
+DOH+=("https://dns.alidns.com/dns-query")
+DOH+=("https://doh.pub/dns-query")
+DOH+=("https://dns.nextdns.io/")
+
+
+[ -f "/usr/bin/gost" ] || {
+  which wget >/dev/null 2>&1
+  [ "$?" -eq 0 ] || exit 1
+  wget -qO- "https://github.com/ginuerzh/gost/releases/download/v2.12.0/gost_2.12.0_linux_amd64.tar.gz" |tar -C /usr/bin -zxv gost && chmod -R 777 /usr/bin/gost || exit 1
+}
+
+i=0;
+for doh in "${DOH[@]}"; do
+  port=$((PORT+i))
+  echo "${port} --> ${doh}"
+  /usr/bin/gost -L="socks5://:${port}?dns=${doh}" >/dev/null 2>&1 &
+  i=$((i+1))
+done
